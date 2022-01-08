81b9c2badc3222dea40f46021c89a9806fc251c9
Clifford Morin <cemorin21@gmail.com>
2022-01-08 01:58:31 -0500

    added write to streams

--- Streams/app.js
+++ Streams/app.js
@@ -3,8 +3,10 @@ const fs = require('fs');
 //Streams are better than fs read because they return data one bit at a time instead of all at once
 //utf-8 makes it so you can see the text otherwise it would just return a buffer
 const readStream = fs.createReadStream(__dirname + '/read-me.txt', 'utf-8');
+const writeStream = fs.createWriteStream(__dirname + '/write-me.txt');
 
 readStream.on('data', (chunk) => {
 	console.log('you received data');
 	console.log(chunk);
+	writeStream.write(chunk);
 });
