bd0ff2037f13f07743bf32e5b8eb0e52d0c0a0a5
Clifford Morin <cemorin21@gmail.com>
2022-01-08 03:58:07 -0500

    addded express basic set up

---
+++ Express/app.js
@@ -0,0 +1,15 @@
+const express = require('express');
+
+const app = express();
+
+app.get('/', (req, res) => {
+    res.send('Home Page')
+});
+app.get('/about', (req, res) => {
+    res.send('About Page')
+});
+app.get('/profile/:name', (req, res) => {
+    res.send('Profile name is ' + req.params.name)
+})
+
+app.listen(3000)
