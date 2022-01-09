fd80d0cdb46f485c7158cc2e251795d6262e7ce7
Clifford Morin <cemorin21@gmail.com>
2022-01-09 04:51:24 -0500

    added post request with ejs

---
+++ Post-Request/app.js
@@ -0,0 +1,33 @@
+const express = require('express');
+const bodyParser = require('body-parser')
+const app = express();
+const urlencodedParser = bodyParser.urlencoded({extended: false});
+
+app.set('view engine', 'ejs');
+
+app.use('/assets', express.static('assets'));
+
+app.get('/', (req, res) => {
+    res.render('index')
+});
+
+app.get('/about', (req, res) => { 
+    res.render('about', { qs: req.query });
+});
+
+app.post('/about', urlencodedParser, (req, res) => { 
+    res.render('about-success', { data: req.body });
+});
+
+app.get('/profile/:name', (req, res) => {
+    const data = {
+        age: 30,
+        job: 'developer',
+        friends: [
+            'sarah', 'jane', 'wilma', 'baba kenya', 'jah man', 'john', 'clifford'
+        ]
+    }
+    res.render('profile', { person: req.params.name, data });
+})
+
+app.listen(3000) 
